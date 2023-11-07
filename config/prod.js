10f285395c3e55cddc629c6acc07295892ee13ed
kimoninanina <nina02929@gmail.com>
2023-11-07 10:52:27 -0800

    fix proxy issues

--- config/prod.js
+++ config/prod.js
@@ -4,9 +4,6 @@ module.exports = {
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
-    // stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-    // stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-    // sendGridKey: process.env.SEND_GRID_KEY,
-    // redirectDomain: process.env.REDIRECT_DOMAIN
+    googleRedirectURI:'https://feedback-collection.onrender.com/auth/google/callback'
   };
 
