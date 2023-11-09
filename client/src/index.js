e95b347f40a391c22dcffc8d214a88d59fd4a304
kimoninanina <nina02929@gmail.com>
2023-11-09 14:20:23 -0800

    Add React Component

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,20 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import App from './components/App';
+import reducers from './reducers';
+
+// Development only axios helpers!
+import axios from 'axios';
+window.axios = axios;
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- client/src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  app.use('/auth/google', createProxyMiddleware({ target: 'http://localhost:8080' }));
-  app.use('/api', createProxyMiddleware({ target: 'http://localhost:8080' }));
-};
