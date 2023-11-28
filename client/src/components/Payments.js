ab673650118c954e0a9cd3d86945090ae2c9edc7
kimoninanina <nina02929@gmail.com>
2023-11-28 10:46:21 -0800

    Front-end code added

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -10,6 +10,7 @@ class Payments extends Component {
         name="Emaily"
         description="$5 for 5 email credits"
         amount={500}
+        //callback function
         token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
